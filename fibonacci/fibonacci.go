4389dbee627b4a0eb232c0009a80549e4fc21795
Xclipse <eclair.cn@gmail.com>
2013-06-07 15:38:36 +0800

    update the fibonacci algorithm

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -2,16 +2,11 @@ package fibonacci
 
 // fibonacci 函数会返回一个返回 int 的函数。
 func Fibonacci() func() int {
-	n, fn1, fn2 := 0, 1, 1
+	fn1, fn2 := 0, 1
 	return func() int {
-		n++
-		if n < 3 {
-			return 1
-		} else {
-			fn2 = fn1 + fn2
-			fn1 = fn2 - fn1
-			return fn2
-		}
+		fn2 = fn1 + fn2
+		fn1 = fn2 - fn1
+		return fn1
 	}
 }
 
